f1293d4dc80e32c6b89716bba62dc50d17c9aee6
Brainiac Rawkib <brainiacrawkib@gmail.com>
2021-02-12 03:51:23 -0400

    :hammer: Updated settings

--- blog/settings.py
+++ blog/settings.py
@@ -185,7 +185,7 @@ MESSAGE_TAGS = {
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_PORT = 587
 EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = (os.getenv('EMAIL_USE_TLS') == 'True')
 
